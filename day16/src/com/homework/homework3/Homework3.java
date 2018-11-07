cc1ad5770224a3798445dc338c026fec19d55143
slhczz <slhczxz@gmail.com>
2018-11-07 09:02:17 +0800

    1.0.0.1

--- day16/src/com/homework/homework3/Homework3.java
+++ day16/src/com/homework/homework3/Homework3.java
@@ -1,15 +1,14 @@
 package com.homework.homework3;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Homework3 {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("输入整数");
-
-
+        int index = 0;
+        Map<Integer,Integer> map = new HashMap<>();
     }
 
 }
--- day16/src/com/homework/homework5/Homework5.java
+++ day16/src/com/homework/homework5/Homework5.java
@@ -12,53 +12,24 @@ public class Homework5 {
         System.out.print("輸入文件夾路徑：");
 
         File file = new File(sc.next());
-
         homeWork(file);
-
     }
-
     public static String homeWork(File f){
         //判断路径是否存在
         boolean flg = f.exists();
         if(!flg){
             return null;
         }
+
         File[] files = f.listFiles();
         if(files!=null){
             for (File file : files) {
-                if(file.isFile()){
-                    System.out.println(file.getName());
-                }else if(file.isDirectory()){
-
-                    System.out.println(file.getName()+"\t");
-                    File[] fi = file.listFiles();
-                    for (File fii: fi) {
-                        if(fii.isFile()){
-                            System.out.println("\t" + fii.getName());
-                        }else if(fii.isDirectory()){
-
-                            File[] fir = fii.listFiles();
-                            System.out.println("\t" + fii.getName()+"\t");
-                            for (File firr:fir) {
-                                if(file.isFile()){
-                                    System.out.println("\t\t"+firr.getName());
-                                }else if(firr.isDirectory()){
-                                    System.out.println("\t\t"+firr.getName());
-                                }else {
-                                    System.out.println("\t\t"+firr.getName());
-                                }
-                            }
-                        }else{
-                            System.out.println("\t" + fii.getName());
-                        }
-                    }
-                }else{
-                    System.out.println(file.getName());
+                System.out.println(file.getAbsolutePath());
+                if(file.isDirectory()){
+                    homeWork(file);
                 }
             }
         }
         return null;
     }
-
-
 }
